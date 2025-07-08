6b8fc89457afbfefb28392f1c231865ad2ca5655
Khizar Malik <khizarmalik2003@gmail.com>
2025-07-08 22:35:06 +0200

    state is smoother now

--- lib/variables.ts
+++ lib/variables.ts
@@ -1,2 +1,2 @@
-export const HOST_URL="https://atsfit-backend-empty-glitter-7552.fly.dev"
-//export const HOST_URL="http://127.0.0.1:8000"
+//export const HOST_URL="https://atsfit-backend-empty-glitter-7552.fly.dev"
+export const HOST_URL="http://127.0.0.1:8000"
